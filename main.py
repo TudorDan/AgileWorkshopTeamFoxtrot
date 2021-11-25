28e87bc27fa6daf1637cdf97bc2b87307e871129
TudorDan <tudor.ist@gmail.com>
2021-11-25 02:39:55 +0200

    Refactor main

--- main.py
+++ main.py
@@ -1,24 +1,4 @@
-from controller import enigma
+from controller import home_controller
 
-
-def print_hi(name):
-    """
-
-    :param name:
-    :return:
-    """
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-    file = open("model/files/decrypted.txt", "rt")
-    data = file.readlines()
-    file.close()
-    print(data)
-
-    with open("model/files/encrypted.txt", "rt") as file2:
-        data2 = file2.read()
-        print(data2)
-
-
-print_hi('PyCharm')
-enigma.show_menu()
+if __name__ == '__main__':
+    home_controller.menu()
