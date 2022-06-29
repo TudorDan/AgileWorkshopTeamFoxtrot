32515b1b9823aaf52e67e8fb9609e9d22a250621
MihaelaNaidin040696 <naidinmihaela76@gmail.com>
2022-06-29 17:21:11 +0300

    [feat] number of students per class

--- controller/students_controller.py
+++ controller/students_controller.py
@@ -7,6 +7,19 @@ def get_all_students():
     display.print_table(students, "Students List:")
 
 
+def get_all_students_per_class():
+    students_class = {}
+    students = data_manager.get_table_from_file("model/files/students.csv")
+    for student in students:
+        if student[1] in students_class:
+            students_class[student[1]] += 1
+        else:
+            students_class.update({student[1]: 1})
+    print("Number of students per class:")
+    for items in students_class.items():
+        print(items)
+
+
 def choose_operation():
     option = display.get_inputs(["Please enter a number: "], "")[0]
     if option == '1':
@@ -17,6 +30,8 @@ def choose_operation():
         display.print_message("Update", True)
     elif option == '4':
         display.print_message("Delete", True)
+    elif option == '5':
+        get_all_students_per_class()
     elif option == '0':
         return False
     else:
@@ -29,7 +44,8 @@ def handle_submenu():
                "List students",
                "Add a new student",
                "Update student",
-               "Delete student"]
+               "Delete student",
+               "Number of students per class"]
     display.print_menu("Student Classes Submenu", options)
 
 
